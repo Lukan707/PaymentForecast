30c4bb4b7ae509e7f975078219341d4d3a78805d
Lukas Andersson <lukan@itu.dk>
2024-01-12 00:23:47 +0100

    Added implementation of datainterface

--- src/main/java/lukan/paymentforecast/Data/DataService.java
+++ src/main/java/lukan/paymentforecast/Data/DataService.java
@@ -1,5 +1,42 @@
 package lukan.paymentforecast.Data;
 
+import java.util.List;
+import lukan.paymentforecast.DomainLogic.*;
+
 public class DataService implements DataServiceInterface {
-    
+    public List<User> getUsers() {
+       throw new UnsupportedOperationException(); 
+    }
+
+    public List<WorkDay> getWorkDays(User user) {
+        throw new UnsupportedOperationException();
+    }
+
+    public List<TimeInterval> getTimeIntervals(WorkDay workDay) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void AddUser(User user) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void RemoveUser(User user) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void AddWorkDay(User user) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void RemoveWorkDay(User user) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void AddTimeInterval(WorkDay workday) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void RemoveTimeInterval(WorkDay workDay) {
+        throw new UnsupportedOperationException();
+    }
 }
