d88abc9d4eaa127de56735dd37b4827bd475b95d
Lukas Andersson <lukan@itu.dk>
2024-01-12 16:43:45 +0100

    Changed interface

--- src/main/java/lukan/paymentforecast/Model/DataService.java
+++ src/main/java/lukan/paymentforecast/Model/DataService.java
@@ -1,5 +1,10 @@
 package lukan.paymentforecast.Model;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import lukan.paymentforecast.DomainLogic.*;
 
@@ -9,12 +14,21 @@ import lukan.paymentforecast.DomainLogic.*;
  */ 
 public class DataService implements DataServiceInterface {
     
-    public DataService() {
-        
-    }
-    
-    public List<User> getUsers() {
-       throw new UnsupportedOperationException(); 
+    public List<User> getUsers() throws FileNotFoundException, IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("./Data/users/userList.csv"));
+
+        List<User> users = new ArrayList<>();
+
+        while(true) {
+            String line = reader.readLine();
+            if (line != null) {
+                break;
+            }
+        }
+
+        reader.close();
+
+        return users;
     }
 
     public List<WorkDay> getWorkDays(User user) {
@@ -25,27 +39,39 @@ public class DataService implements DataServiceInterface {
         throw new UnsupportedOperationException();
     }
 
-    public void AddUser(User user) {
+    public List<Supplement> getSupplements(User user) {
         throw new UnsupportedOperationException();
     }
 
-    public void RemoveUser(User user) {
+    public void addUser(User user) {
         throw new UnsupportedOperationException();
     }
 
-    public void AddWorkDay(User user) {
+    public void removeUser(User user) {
         throw new UnsupportedOperationException();
     }
 
-    public void RemoveWorkDay(User user) {
+    public void addWorkDay(User user) {
         throw new UnsupportedOperationException();
     }
 
-    public void AddTimeInterval(WorkDay workday) {
+    public void removeWorkDay(User user) {
         throw new UnsupportedOperationException();
     }
 
-    public void RemoveTimeInterval(WorkDay workDay) {
+    public void addTimeInterval(WorkDay workday) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void removeTimeInterval(WorkDay workDay) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void addSupplement(User user) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void removeSupplement(User user) {
         throw new UnsupportedOperationException();
     }
 }
--- src/main/java/lukan/paymentforecast/Model/DataServiceInterface.java
+++ src/main/java/lukan/paymentforecast/Model/DataServiceInterface.java
@@ -1,16 +1,21 @@
 package lukan.paymentforecast.Model;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
 import lukan.paymentforecast.DomainLogic.*;
 
 public interface DataServiceInterface {
-    public List<User> getUsers();
-    public List<WorkDay> getWorkDays(User user);
+    public List<User> getUsers() throws FileNotFoundException, IOException;
+    public List<WorkDay> getWorkDays(User user) throws FileNotFoundException;
     public List<TimeInterval> getTimeIntervals(WorkDay workDay);
-    public void AddUser(User user);
-    public void RemoveUser(User user);
-    public void AddWorkDay(User user);
-    public void RemoveWorkDay(User user);
-    public void AddTimeInterval(WorkDay workDay);
-    public void RemoveTimeInterval(WorkDay workDay);
+    public List<Supplement> getSupplements(User user);
+    public void addUser(User user);
+    public void removeUser(User user);
+    public void addWorkDay(User user);
+    public void removeWorkDay(User user);
+    public void addTimeInterval(WorkDay workDay);
+    public void removeTimeInterval(WorkDay workDay);
+    public void addSupplement(User user);
+    public void removeSupplement(User user);
 }
