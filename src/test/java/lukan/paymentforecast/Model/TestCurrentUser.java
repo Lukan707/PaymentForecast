85d6770958aefa594004d72acdc83f2fb8368b17
Lukas Andersson <lukan@itu.dk>
2024-08-04 13:38:42 +0200

    Added printing to tets that failed

--- src/test/java/lukan/paymentforecast/Model/TestCurrentUser.java
+++ src/test/java/lukan/paymentforecast/Model/TestCurrentUser.java
@@ -13,11 +13,6 @@ import org.junit.jupiter.api.Test;
 import lukan.paymentforecast.Domain.User;
 import lukan.paymentforecast.Domain.Exceptions.NoCurrentUser;
 
-/* 
-    TODO: Change such testing is not done on the actual files of the program?
-    Or just let it be, as the program is to be compiled and move elsewhere when done.
-*/
-
 public class TestCurrentUser {
 
     @Test
@@ -52,23 +47,23 @@ public class TestCurrentUser {
         assertEquals(testUser.hourlySalary, response.hourlySalary);
     }
 
-    // @Test
-    // public void TestGetCurrentUserThrowsNoCurrentUserException() {
-    //     // Arrange
-    //     File file = new File("./Data/users/currentUSer.csv");
-    //     DataService data = new DataService();
+    @Test
+    public void TestGetCurrentUserThrowsNoCurrentUserException() {
+        // Arrange
+        File file = new File("./Data/users/currentUSer.csv");
+        DataService data = new DataService();
 
-    //     try {
-    //         file.delete();
-    //         file.getParentFile().mkdirs();
-    //         file.createNewFile();
-    //     } catch (IOException e) {
+        try {
+            file.delete();
+            file.getParentFile().mkdirs();
+            file.createNewFile();
+        } catch (IOException e) {
+            System.out.println("The test failed due to an ioexception");
+        }
 
-    //     }
-
-    //     // Assert
-    //     assertThrows(NoCurrentUser.class, () -> data.getCurrentUser());
-    // }
+        // Assert
+        assertThrows(NoCurrentUser.class, () -> data.getCurrentUser());
+    }
 
     @Test
     public void TestSetCurrentUser() {
